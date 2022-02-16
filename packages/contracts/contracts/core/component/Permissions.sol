80cf50102942a095a140877208a5596cbf963cd1
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2022-02-16 17:44:29 +0100

    created PermissionedRelayRecipient
     to separate Permissions from BaseRelayRecipient traits

--- packages/contracts/contracts/core/component/Permissions.sol
+++ packages/contracts/contracts/core/component/Permissions.sol
@@ -5,7 +5,6 @@
 pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@opengsn/contracts/src/BaseRelayRecipient.sol";
 
 import "./../IDAO.sol";
 
@@ -13,15 +12,15 @@ import "./../IDAO.sol";
 /// @author Samuel Furter - Aragon Association - 2022
 /// @notice This contract can be used to include the modifier logic(so contracts don't repeat the same code) that checks permissions on the dao.
 /// @dev When your contract inherits from this, it's important to call __Initialize_DAO_Permission with the dao address.
-abstract contract Permissions is Initializable, BaseRelayRecipient {
+abstract contract Permissions is Initializable {
     
     /// @dev Every component needs DAO at least for the permission management. See 'auth' modifier.
     IDAO internal dao;
     
     /// @dev Auth modifier used in all components of a DAO to check the permissions.
     /// @param _role The hash of the role identifier
-    modifier auth(bytes32 _role)  {
-        require(dao.hasPermission(address(this), _msgSender(), _role, _msgData()), "component: auth");
+    modifier auth(bytes32 _role) virtual {
+        require(dao.hasPermission(address(this), msg.sender, _role, msg.data), "component: auth");
         _;
     }
 
